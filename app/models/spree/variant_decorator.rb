3f9adc3ae05a204df9162f97aff27c668b109cb4
Laurens Nienhaus <l.nienhaus@gmail.com>
2013-08-13 14:21:30 +0200

    Use parse_price helper when assigning shipping_rate

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,3 +1,7 @@
 Spree::Variant.class_eval do
   attr_accessible :shipping_rate
+
+  def shipping_rate=(shipping_rate)
+    self[:shipping_rate] = parse_price(shipping_rate) if shipping_rate.present?
+  end
 end
