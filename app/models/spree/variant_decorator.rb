3e209847f56abfe91cf20f95d900ac7c8af92540
Laurens Nienhaus <l.nienhaus@gmail.com>
2014-01-19 22:34:24 +0100

    Allow empty shipping rates

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -2,6 +2,10 @@ Spree::Variant.class_eval do
   attr_accessible :shipping_rate
 
   def shipping_rate=(shipping_rate)
-    self[:shipping_rate] = parse_price(shipping_rate) if shipping_rate.present?
+    if shipping_rate.present?
+      self[:shipping_rate] = parse_price(shipping_rate)
+    else
+      super
+    end
   end
 end
